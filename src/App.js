61307821bc4a332294e07a65d36a09bebd69686b
Nicoyarce <nicoyarce@gmail.com>
2021-05-31 22:57:06 -0400

    Primer commit y maqueta pokedex

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+import { Fragment } from "react";
+import Pokedex from "./components/Pokedex";
+
+function App() {
+    return (
+        <Fragment>
+            <Pokedex></Pokedex>
+        </Fragment>
+    );
+}
+
+export default App;
---
+++ src/components/Pokedex.js
@@ -0,0 +1,59 @@
+const Pokedex = () => {
+    return (
+        <div class="pokedex">
+            <div class="cuerpos">
+                <div class="tapa-izq">
+                    <div class="bordes">
+                        <div class="borde-izq">
+                            <div class="circulo-cristal"></div>
+                            <div class="grupoluz-circulo circulo-rojo"></div>
+                            <div class="luz-circulo circulo-amarillo"></div>
+                            <div class="luz-circulo circulo-verde"></div>
+                        </div>
+                        <div class="borde-izq-chico"></div>
+                    </div>
+                    <div class="marco-pantalla">
+                        <div class="pantalla pantalla-grande"></div>
+                    </div>
+                    <div class="grupo-botones-izq">
+                        <div class="boton-circulo circulo-izq"></div>
+                        <div class="mini-pantalla"></div>
+                        <div class="cruceta"></div>
+                    </div>
+                </div>
+                <div class="medio"></div>
+                <div class="tapa-der">
+                    <div class="borde-der"></div>
+                    <div class="interior-tapa-der">
+                        <div class="pantalla pantalla-mediana"></div>
+                        <div class="grupo-botones-der">
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                            <div class="boton-azul"></div>
+                        </div>
+                        <div class="grupo-botones-finales">
+                            <div class="grupo-botones-blanco">
+                                <div class="boton-blanco"></div>
+                                <div class="boton-blanco"></div>
+                            </div>
+                            <div class="boton-circulo circulo-amarillo"></div>
+                        </div>
+                        <div class="grupo-pantallas-chicas">
+                            <div class="pantalla pantalla-chica"></div>
+                            <div class="pantalla pantalla-chica"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Pokedex;
