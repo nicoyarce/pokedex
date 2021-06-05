a11f6373472b9b4f1895bc3667e753e32f0d40d8
Nicoyarce <nicoyarce@gmail.com>
2021-06-05 17:31:40 -0400

    Agrega bottom navbar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ function App() {
                 <div className="texto-titulo">Pokedex de Nicoyarce</div>
             </div>
             <Pokedex></Pokedex>
+            <div className="navbar">
+                <a
+                    href="https://nicoyarce.github.io/me/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Creado por Nicoyarce
+                </a>
+                <a
+                    href="https://trello.com/b/a9n6v5QE/pokedex"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Trello - ToDo
+                </a>
+            </div>
         </Fragment>
     );
 }
